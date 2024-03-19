dbd37dc71275ec3b06f74c9365a60e7f22dec199
marekwadinger <marekwadinger5506@gmail.com>
2024-03-19 11:47:40 +0900

    ADD: examples as performance evaluations

---
+++ functions/plot.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.axes import Axes
+
+from .preprocessing import normalize as _normalize
+
+
+def plot_chd(
+    datas: list[np.ndarray],
+    y_true: list[float] | np.ndarray | None = None,
+    labels: list[str] | None = None,
+    idx_start: int | None = None,
+    idx_end: int | None = None,
+    idx_in_start: int | None = None,
+    idx_in_end: int | None = None,
+    normalize: bool = False,
+):
+    fig, axs = plt.subplots(len(datas), 1, sharex=True)
+
+    idx_start = 0 if idx_start is None else idx_start
+    idx_end = len(datas[0]) if idx_end is None else idx_end
+    x = range(idx_start, idx_end)
+
+    if idx_in_start and idx_in_end:
+        x_in = range(idx_in_start, idx_in_end)
+    else:
+        x_in = None
+
+    if labels is None:
+        labels = [""] * len(datas)
+
+    for ax, data, label in zip(axs, datas, labels):
+        if isinstance(ax, Axes):
+            ax.plot(x, data[idx_start:idx_end], label=label)
+            if y_true is not None:
+                for i in y_true:
+                    ax.axvline(i, color="tab:red", linestyle="--")
+            if normalize:
+                ax_norm = ax.twinx()
+                ax_norm.plot(  # type: ignore
+                    _normalize(data[idx_start:idx_end]),
+                    label=label + " (norm)",
+                )
+            ax.legend([label])
+            ax.grid(True)
+
+            if x_in:
+                # Add inlay plot
+                inlay_ax = ax.inset_axes(
+                    (0.1, 0.4, 0.6, 0.6)
+                )  # Adjust the position and size of the inlay plot
+                inlay_ax.plot(x_in, data[idx_in_start:idx_in_end], label=label)
+                inlay_ax.grid(True)
+                inlay_ax.set_yticklabels([])
+
+    return fig, axs
