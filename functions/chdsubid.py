3e977a08c79b190fa862e44066fde77c9a62f18b
Marek Wadinger <marekwadinger5506@gmail.com>
2024-07-08 16:22:17 +0200

    UPDATE: auxiliary functions

--- functions/chdsubid.py
+++ functions/chdsubid.py
@@ -13,7 +13,11 @@ from .rolling import Rolling
 
 
 # # Default parameters
-def get_default_timedelays(h: int, n_features_max: int) -> tuple[int, int]:
+def get_default_timedelays(
+    h: int, n_features_max: int | None = None
+) -> tuple[int, int]:
+    if n_features_max is None:
+        return h, 1
     if h < n_features_max:
         h_ = h
         step = 1
@@ -23,7 +27,7 @@ def get_default_timedelays(h: int, n_features_max: int) -> tuple[int, int]:
     return h_, step
 
 
-def get_default_rank(X):
+def get_default_rank(X, noise_variance: float | None = None):
     """Get default rank for the given data matrix
 
     Args:
@@ -35,10 +39,20 @@ def get_default_rank(X):
     References:
         [1] Gavish, M., and Donoho L. D. (2014). The Optimal Hard Threshold for Singular Values is 4/sqrt(3). IEEE Transactions on Information Theory 60.8 (2014): 5040-5053. doi:[10.1109/TIT.2014.2323359](https://doi.org/10.1109/TIT.2014.2323359).
     """
+    n, m = X.shape
+    beta = m / n
     s = np.linalg.svd(X.T, compute_uv=False)
-    beta = X.shape[1] / X.shape[0]
-    omega = 0.56 * beta**3 - 0.95 * beta**2 + 1.82 * beta + 1.43
-    r = sum(s > omega * np.median(s))
+    if noise_variance is None:
+        omega = 0.56 * beta**3 - 0.95 * beta**2 + 1.82 * beta + 1.43
+        tau = omega * np.median(s)
+    else:
+        lambda_opt = np.sqrt(
+            2 * (beta + 1)
+            + (8 * beta) / ((beta + 1) + np.sqrt(beta**2 + 14 * beta + 1))
+        )
+
+        tau = lambda_opt * np.sqrt(n * noise_variance)
+    r = sum(s > tau)
     return r
 
 
